266ce17a81527c9fb559e54f84ff78f3cfefc464
912-Cireap-Bogdan <bogdan.cireap@gmail.com>
2023-04-04 16:29:26 +0300

    Lab3 in class testing code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,11 +19,13 @@ public class Main {
         Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         UI consola = new UI(service);
 
-        service.saveStudent("999","student1",932);
-        service.saveTema("999","tema random",9,8);
-        int result = service.saveNota("999","1",9,1,"feedback");
+//        int result_save = service.saveStudent("999","student1",932);
+        int result_tema = service.saveTema("999","tema random",9,8);
+        int result_tema2 = service.saveTema("999","tema random",9,8);
+//        int result = service.saveNota("999","1",9,1,"feedback");
 
-
+        System.out.println(result_tema);
+        System.out.println(result_tema2);
         consola.run();
 
         //PENTRU GUI
---
+++ src/test/java/TestAddAssignment.java
@@ -0,0 +1,55 @@
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+import static org.junit.Assert.assertEquals;
+
+public class TestAddAssignment {
+
+    Validator<Student> studentValidator = new StudentValidator();
+    Validator<Tema> temaValidator = new TemaValidator();
+    Validator<Nota> notaValidator = new NotaValidator();
+
+    StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+    TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+    NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+    Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+    @Test
+    public void testAddAssignment(){
+
+        //test case 1 id >0 and a number
+        assertEquals(0,service.saveTema("999","tema random",9,8));
+        assertEquals(1,service.saveTema("-999","tema random",9,8));
+        assertEquals(1,service.saveTema("a","tema random",9,8));
+
+
+    }
+    @Test
+    public void testAddAssignmentDescription(){
+
+        //test case 2 description must not be empty
+        assertEquals(1,service.saveTema("999","",9,8));
+        assertEquals(0,service.saveTema("999"," ",9,8));
+        assertEquals(0,service.saveTema("999","t",9,8));
+    }
+
+    @Test
+    public void testAddAssignmentDeadline(){
+        // deadline > startline
+        assertEquals(1,service.saveTema("999","tema random",7,8));
+        assertEquals(0,service.saveTema("999","tema random",9,8));
+        assertEquals(0,service.saveTema("999","tema random",7,7));
+
+    }
+
+}
